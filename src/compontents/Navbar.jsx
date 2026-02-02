2e8a29fc8872f806e5d844cbcb1194b498a8426d
Navdeep <digitalnavtech6@gmail.com>
2026-02-02 13:54:24 +0200

    Add constants/index.js

--- src/compontents/Navbar.jsx
+++ src/compontents/Navbar.jsx
@@ -1,3 +1,4 @@
+import { navLinks } from "../constants"
 
 
 const Navbar = () => {
@@ -6,15 +7,7 @@ const Navbar = () => {
     <nav>
         <img src='/logo.svg' alt='Apple Logo '/>
         <ul>
-            {[
-                {label: "Store"},
-                {label: "Mac"},
-                {label: "iPhone"},
-                {label: "Watch"},
-                {label: "Vision"},
-                {label: "AirPods"}
-
-            ].map(({label})=>(
+            {navLinks.map(({label})=>(
                 <li key={label}>
                     <a href={label}> {label}</a>
                 </li>
